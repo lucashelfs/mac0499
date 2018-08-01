706503520dd9ff37d87ce622f5f0c6284eeb1220
lucashelfs <lucashelfstein@gmail.com>
2018-08-01 15:52:58 -0300

    venv removed

--- códigos/clam/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/helfs/Documentos/grad/manual-classifier-helper/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- códigos/clam/venv/lib/python3.6/sre_compile.py
+++
@@ -1,1 +0,0 @@
-/home/helfs/anaconda3/lib/python3.6/sre_compile.py
