599d613c60648b737a3fb68f68406f9339fe3f45
lucashelfs <lucashelfstein@gmail.com>
2018-08-16 19:08:55 -0300

    Integrando com 'django-material'

--- códigos/clam/mchelper/settings.py
+++ códigos/clam/mchelper/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'material',
 ]
 
 MIDDLEWARE = [
@@ -77,14 +78,18 @@ WSGI_APPLICATION = 'mchelper.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'nomedodb',
-       'USER': 'donododb',
-       'PASSWORD': 'newpassword',
-       'HOST': 'localhost',
-       'PORT': ''
-   }
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #     'NAME': 'nomedodb',
+   #     'USER': 'donododb',
+   #     'PASSWORD': 'newpassword',
+   #     'HOST': 'localhost',
+   #     'PORT': ''
+   # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
